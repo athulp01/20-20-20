22a13b81df05715461819ee38680cc049b6f9dc0
Athul P <athul929@gmail.com>
2021-06-06 00:56:00 +0530

    fix signal handling

--- main.c
+++ main.c
@@ -20,36 +20,37 @@ static void set_screen_state(unsigned short state) {
   return;
 }
 
+static void reset() {
+  if (is_reset) {
+    is_reset = 0;
+    system("notify-send \"Timer reset\" -u normal -t 1000");
+  }
+}
+
 //Turn off screen after every timeout seconds for interval seconds;
 //timeout and interval are in seconds
 static void lock(unsigned int timeout, unsigned int interval) {
   sleep(timeout - 60);
-  if (is_reset) {
-    is_reset = 0;
-    system("notify-send \"Timer reset\" -u normal -t 1000");
-    return;
-  }
+  reset(); //signal will wake this sleep, if user presses the key
   system("notify-send \"Screen will be turned off in 1 minute. Pass SIGUSR1 to "
          "reset the timer\" -u normal -t 5000");
   sleep(60);
-  if (is_reset) {
-    is_reset = 0;
-    system("notify-send \"Timer reset\" -u normal -t 1000");
-    return;
-  }
+  reset();
   set_screen_state(0);
   sleep(interval);
   set_screen_state(1);
+  reset();
   return;
 }
 
-static void reset(int sig) {
+
+static void handle_sigusr1(int sig) {
   is_reset = 1;
   return;
 }
 
 int main() {
-  signal(SIGUSR1, reset);
+  signal(SIGUSR1, handle_sigusr1);
   while (1) {
     lock(1200, 20);
   }
