853d042afc02c17f7316c6a179174995c3fd9b90
Athul P <athul929@gmail.com>
2021-06-20 22:18:46 +0530

    condition to standby

--- main.c
+++ main.c
@@ -8,15 +8,12 @@
 
 static unsigned char is_reset;
 
-static void set_screen_state(unsigned short state) {
-  Display *dpy;
-  if (!(dpy = XOpenDisplay(0)))
-    errx(1, "cannot open display '%s'", XDisplayName(0));
-  DPMSEnable(dpy);
+static void set_screen_state(unsigned short state, Display *dpy) {
   CARD16 standby;
   BOOL onoff;
   DPMSInfo(dpy, &standby, &onoff);
-  if (onoff == DPMSModeStandby) {
+  printf("%d %d\n", onoff, standby);
+  if (standby == 0) {
     if (state)
       DPMSForceLevel(dpy, DPMSModeOn);
     else
@@ -26,26 +23,31 @@ static void set_screen_state(unsigned short state) {
   return;
 }
 
-static void reset() {
+static int reset() {
   if (is_reset) {
     is_reset = 0;
     system("notify-send \"Timer reset\" -u normal -t 1000");
+    return 1;
   }
+  return 0;
 }
 
 // Turn off screen after every timeout seconds for interval seconds;
 // timeout and interval are in seconds
-static void lock(unsigned int timeout, unsigned int interval) {
+static void lock(unsigned int timeout, unsigned int interval, Display *dpy) {
   sleep(timeout - 60);
-  reset(); // signal will wake this sleep, if user presses the key
+  if (reset())
+    return; // signal will wake this sleep, if user presses the key
   system("notify-send \"Screen will be turned off in 1 minute. Pass SIGUSR1 to "
          "reset the timer\" -u normal -t 5000");
   sleep(60);
-  reset();
-  set_screen_state(0);
+  if (reset())
+    return; // signal will wake this sleep, if user presses the key
+  set_screen_state(0, dpy);
   sleep(interval);
-  set_screen_state(1);
-  reset();
+  set_screen_state(1, dpy);
+  if (reset())
+    return; // signal will wake this sleep, if user presses the key
   return;
 }
 
@@ -55,7 +57,12 @@ static void handle_sigusr1(int sig) {
 }
 
 int main() {
-  printf("dafa");
+  Display *dpy;
+  if (!(dpy = XOpenDisplay(0)))
+    errx(1, "cannot open display '%s'", XDisplayName(0));
+  DPMSEnable(dpy);
   signal(SIGUSR1, handle_sigusr1);
-  set_screen_state(1);
+  while (1) {
+    lock(1200, 20, dpy);
+  }
 }
